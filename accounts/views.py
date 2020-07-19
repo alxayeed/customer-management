c1352b31292211e3b8782932fb44dd61a71d54fe
alxayeed <alxayeed@gmail.com>
2020-07-19 10:35:12 +0600

    templates/accounts folder created.home, customer, product page created by template inheritence

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'accounts/index.html')
 
 def customer(request):
-    return HttpResponse('Customer Page')
+    return render(request, 'accounts/customer.html')
 
 def product(request):
-    return HttpResponse('Product Page')
+    return render(request, 'accounts/product.html')
