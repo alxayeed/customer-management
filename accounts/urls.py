56d3814a4d7618d93f450d6c58108ab62e77aff8
alxayeed <alxayeed@gmail.com>
2020-07-19 09:22:07 +0600

    urls and views configured for accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('',home, name='home'),
+    path('product/',product, name='product'),
+    path('customer/',customer, name='customer'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def customer(request):
+    return HttpResponse('Customer Page')
+
+def product(request):
+    return HttpResponse('Product Page')
