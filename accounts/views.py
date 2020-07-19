cbb09e1b12df226ca5f9fbcf2bcbbfc430a9f6f6
alxayeed <alxayeed@gmail.com>
2020-07-19 15:28:12 +0600

    data passed to templates through view

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/index.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_orders': total_orders , 'delivered': delivered, 'pending' : pending}
+    
+    return render(request, 'accounts/index.html', context)
 
 def customer(request):
     return render(request, 'accounts/customer.html')
 
 def product(request):
-    return render(request, 'accounts/product.html')
+    products = Product.objects.all()
+
+    context = {'products': products}
+    return render(request, 'accounts/product.html', context)
