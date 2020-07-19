552e20d45fd122c1d0d215b3d0b9c1dd656dc960
alxayeed <alxayeed@gmail.com>
2020-07-19 16:11:36 +0600

    link updated to dynamically pass data from home to individual customer.navbar links updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('',home, name='home'),
     path('product/',product, name='product'),
-    path('customer/<int:pk>',customer, name='customer'),
+    path('customer/<int:pk>/',customer, name='customer'),
 ]
