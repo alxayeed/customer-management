b4c205443f22e27406a6fb1a2c3be229ae328be4
alxayeed <alxayeed@gmail.com>
2020-07-19 20:04:29 +0600

    create,update, delete for order done

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ from .views import *
 urlpatterns = [
     path('',home, name='home'),
     path('product/',product, name='product'),
-    path('customer/<int:pk>/',customer, name='customer'),
+    path('customer/<int:pk>/', customer, name='customer'),
+    path('create-order/',createOrder, name="create_order"),
+    path('update-order/<int:pk>/',updateOrder, name="update_order"),
+    path('delete-order/<int:pk>/',deleteOrder, name="delete_order"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -32,3 +33,40 @@ def product(request):
 
     context = {'products': products}
     return render(request, 'accounts/product.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'accounts/order_form.html', context)
+    
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    context = {'form': form}
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    context = {'order': order}
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    return render(request, 'accounts/delete.html', context)
