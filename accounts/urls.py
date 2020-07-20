5be006ae336e5bd5c2038504a655022ee3ba2182
alxayeed <alxayeed@gmail.com>
2020-07-20 21:08:20 +0600

    inline formset added to create order for individual customer

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',home, name='home'),
     path('product/',product, name='product'),
     path('customer/<int:pk>/', customer, name='customer'),
-    path('create-order/',createOrder, name="create_order"),
+    path('create-order/<int:pk>/',createOrder, name="create_order"),
     path('update-order/<int:pk>/',updateOrder, name="update_order"),
     path('delete-order/<int:pk>/',deleteOrder, name="delete_order"),
 ]
