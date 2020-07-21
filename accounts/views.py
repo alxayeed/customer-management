a0f4ff04bd84202b5849dc3c09f4a731838c1607
alxayeed <alxayeed@gmail.com>
2020-07-21 19:17:53 +0600

    Login functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 from .models import *
@@ -41,6 +41,18 @@ def createUser(request):
     return render(request, 'accounts/signup.html', context)
 
 def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        else:
+            messages.info(request,'Username or Password is incorrect')
+
     return render(request, 'accounts/login.html')
 
 def logoutUser(request):
